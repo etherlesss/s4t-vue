808e492457773a470a28db5d6a2ed46ef76f646b
Martín Santana <martin.santana.ibacache@gmail.com>
2023-10-28 08:45:44 -0300

    Validacion de formularios
    Se añadieron las validaciones con el uso de yup y veevalidate

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,50 +1,53 @@
 <template>
     <div class="d-flex flex-row">
         <LoginSidebar />
-        <main class="form-content">
-            <div class="main-content container-md w-75">
-                <h2 class="w-25">Iniciar sesión</h2>
-                <form class="row g-3">
-                    <div class="mb-3 col-12">
-                        <label for="exampleInputEmail1" class="form-label">Correo electrónico</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        <div id="emailHelp" class="form-text">Nunca compartiremos su correo con terceros.</div>
-                    </div>
-                    <div class="mb-3 col-12">
-                        <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
-                        <div id="passwordHelp" class="form-text">Nunca le preguntaremos por su contraseña.</div>
-                    </div>
-                    <div class="mb-3 col-12">
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Recuérdame</label>
-                        </div>
-                        <label class="form-check-label" for="exampleCheck1">¿No tienes cuenta? Registrate 
-                            <router-link to="/signup">aquí</router-link>.</label>
-                    </div>
-                    <div class="mb-3 col-12">
-                        <button type="submit" class="btn btn-primary main-btn">Iniciar sesión</button>
-                    </div>
-                </form>
-            </div>
-        </main>
+        <loginForm :schema="formSchema"/>
     </div>
+    
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import LoginSidebar from '@/components/sidebars/Login.vue';
+import loginForm from '@/Forms/loginForm.vue';
+import * as Yup from 'yup';
 
 export default defineComponent({
     name: 'LoginView',
     components: {
-        LoginSidebar
+        LoginSidebar,
+        loginForm
     },
     // SEO
     mounted() {
         document.title = 'S4T | Inicio de sesión';
-    },
+    }, 
+    data:() => {
+        const formSchema={
+            fields:[
+                {
+                    label:'Correo Electronico',
+                    name: 'email',
+                    as: 'input',
+                    class:'form-control',
+                    help:'Nunca compartiremos su correo con terceros.',
+                    rules: Yup.string().email("Ingresar un Correo Valido").required("Campo Obligatorio")
+                },
+                {
+                    label:'Contraseña',
+                    name: 'password',
+                    as:'input',
+                    type:'password',
+                    class:'form-control',
+                    help:'Nunca le preguntaremos por su contraseña.',
+                    rules: Yup.string().required("Campo Obligatorio")
+                },
+            ],
+        };
+        return{
+            formSchema,
+        };
+    }
 });
 </script>
 
@@ -52,7 +55,9 @@ export default defineComponent({
 .form-content {
     flex-grow: 1;
 }
-
+.page{
+    display: flex;
+}
 .form-title {
     font-size: 24px;
     width: 100%;
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,69 +1,81 @@
 <template>
     <div class="d-flex flex-row">
-        <SignupSidebar />
-        <main class="form-content">
-            <div class="main-content container-md w-75">
-                <h2 class="w-25 form-title">Registro</h2>
-                <form class="row g-3" id="formSignUp">
-                    <div class="col-md-6">
-                        <label for="exampleInputName1" class="form-label">Nombre</label>
-                        <input type="text" class="form-control" name="exampleInputName1" id="exampleInputName1"
-                            aria-describedby="emailHelp">
-                    </div>
-                    <div class="col-md-6">
-                        <label for="exampleInputLastName1" class="form-label">Apellido</label>
-                        <input type="text" class="form-control" name="exampleInputLastName1" id="exampleInputLastName1"
-                            aria-describedby="emailHelp">
-                    </div>
-                    <div class="col-12">
-                        <label for="exampleInputAddress1" class="form-label">Dirección</label>
-                        <input type="text" class="form-control" name="exampleInputAddress1" id="exampleInputAddress1"
-                            aria-describedby="emailHelp">
-                    </div>
-                    <div class="col-12">
-                        <label for="exampleInputEmail1" class="form-label">Correo electrónico</label>
-                        <input type="email" class="form-control" name="exampleInputEmail1" id="exampleInputEmail1"
-                            aria-describedby="emailHelp">
-                    </div>
-                    <div class="col-md-6">
-                        <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-                        <input type="password" class="form-control" name="exampleInputPassword1" id="exampleInputPassword1">
-                    </div>
-                    <div class="col-md-6">
-                        <label for="exampleInputConfirmPassword1" class="form-label">Verificar contraseña</label>
-                        <input type="password" class="form-control" name="exampleInputConfirmPassword1"
-                            id="exampleInputConfirmPassword1">
-                    </div>
-                    <div class="col-12">
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Acepto los <a href="#">términos y
-                                    condiciones</a>.</label>
-                        </div>
-                        <label class="form-check-label" for="exampleCheck1">¿Ya tienes cuenta? Inicia sesión <router-link to="/login">aquí</router-link>.</label>
-                    </div>
-                    <div class="col-12">
-                        <button type="submit" class="btn btn-primary main-btn" id="btnSignUp">Registrarse</button>
-                    </div>
-                </form>
-            </div>
-        </main>
+        <SignupSidebar/>
+        <signupForm :schema="formSchema"/>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import * as Yup from 'yup';
 import SignupSidebar from '@/components/sidebars/Signup.vue';
+import signupForm from '@/Forms/signupForm.vue';
 
 export default defineComponent({
     name: 'SignupView',
     components: {
-        SignupSidebar
+        SignupSidebar,
+        signupForm
     },
     // SEO
     mounted() {
         document.title = 'S4T | Creación de cuenta';
     },
+    data: () => {
+        const formSchema = {
+            fields: [
+                {
+                    label: 'Usuario',
+                    name: 'User',
+                    as: 'input',
+                    class: 'form-control',
+                    rules: Yup.string().max(30, "El usuario no debe tener más de 30 caracteres").min(3, "El usuario debe tener al menos 3 caracteres").required("Campo Obligatorio")
+                },
+                {
+                    label: 'Rut (sin puntos y con guión)',
+                    name: 'Rut',
+                    as: 'input',
+                    class: 'form-control',
+                    rules: Yup.string().matches(/^[0-9]+-[0-9kK]{1}$/, 'Ingresa un Rut válido').required('Campo Obligatorio')
+                },
+                {
+                    label: 'Correo Electronico',
+                    name: 'email',
+                    as: 'input',
+                    class: 'form-control',
+                    help: 'Nunca compartiremos su correo con terceros.',
+                    rules: Yup.string().email("Ingresar un Correo Valido").required("Campo Obligatorio")
+                },
+                {
+                    label: 'Contraseña',
+                    name: 'Password',
+                    as: 'input',
+                    type:'password',
+                    class: 'form-control',
+                    rules: Yup.string()
+                        .matches(
+                            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/,
+                            'La contraseña debe tener al menos 8 caracteres, incluyendo al menos una letra mayúscula, una letra minúscula y un número'
+                        )
+                        .required('Campo Obligatorio')
+                },
+                {
+                    label: 'Confirmar contraseña',
+                    name: 'ConfirmPassword',
+                    as: 'input',
+                    type:'password',
+                    class: 'form-control',
+                    rules: Yup.string()
+                        .oneOf([Yup.ref('Password')], 'Las contraseñas no coinciden')
+                        .required('Campo Obligatorio')
+                },
+                
+            ],
+        };
+        return{
+            formSchema
+        };
+    }
 });
 </script>
 
